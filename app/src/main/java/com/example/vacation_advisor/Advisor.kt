1ce37d1ab625ddd44575d3d108d07445275696b7
Jack DAngelo <jmd421@georgetown.edu>
2024-09-17 23:24:09 -0400

    Prettification

--- app/src/main/java/com/example/vacation_advisor/Advisor.kt
+++ app/src/main/java/com/example/vacation_advisor/Advisor.kt
@@ -1,32 +1,18 @@
 package com.example.vacation_advisor
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Slider
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -46,122 +32,275 @@ fun FindDestination(
     var dialogStatus by remember { mutableStateOf(false) }
     val uiState by bakingViewModel.uiState.collectAsState()
 
-    Column(
-        modifier = Modifier.padding(32.dp)
-    ) {
-        Text(
-            text = "Vacation Destination Advisor",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(32.dp)
-        )
-
-        Slider(
-            value = month.toFloat(),
-            onValueChange = { month = it.toInt() },
-            valueRange = 1f..12f,
-            steps = 11
-        )
-        Text("Month: $month")
-
-        Box {
-            Text(budget, modifier = Modifier.clickable { expanded = !expanded })
-            DropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false }
-            ) {
-                budgetOptions.forEach { selectionOption ->
-                    DropdownMenuItem(
-                        text = { Text(selectionOption) },
-                        onClick = {
-                            budget = selectionOption
-                            expanded = false
-                        }
+    // Use a Box with constrained height for scrollable content
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFFf1c40f), // Tropical yellow
+                        Color(0xFFf39c12), // Orange
+                        Color(0xFFe74c3c)  // Warm red
                     )
+                )
+            )
+            .paint(
+                painter = painterResource(id = R.drawable.vacation_background), // Correct resource reference
+                contentScale = ContentScale.Crop
+            )
+    ) {
+        // Constrain height to avoid infinite height issues
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(2.dp)
+                .verticalScroll(rememberScrollState())
+                .heightIn(max = 900.dp) // Constrain height here
+        ) {
+            // Title Panel
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(4.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.surface.copy(alpha = 0.8f),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .padding(8.dp)
+            ) {
+                Text(
+                    text = "Vacation Destination Advisor",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.align(Alignment.Center),
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+            }
+
+            // Slider Panel
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.surface.copy(alpha = 0.8f),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .padding(8.dp)
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("Select Month", style = MaterialTheme.typography.bodyLarge)
+                    Slider(
+                        value = month.toFloat(),
+                        onValueChange = { month = it.toInt() },
+                        valueRange = 1f..12f,
+                        steps = 11
+                    )
+                    Text("Month: $month")
                 }
             }
-        }
 
-        typeOptions.forEach { text ->
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                RadioButton(
-                    selected = (text == typeSelectedOption),
-                    onClick = { typeSelectedOption = text }
-                )
-                Text(text)
-            }
-        }
-
-        Button(
-            onClick = {
-                dialogStatus = true
-
-            },
-        ) {
-            Text("Find My Destination")
-        }
-
-        // display an alert dialog to warn the user about AI limitations (Gemini can make mistakes. Check important info.)
-        if (dialogStatus) {
-            AlertDialog(
-                onDismissRequest = { dialogStatus = true },
-                title = { Text("AI is not perfect") },
-                text = {
-                    Text(
-                        "Gemini can make mistakes. Check important info."
+            // Budget Panel
+            // Budget Panel
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.surface.copy(alpha = 0.8f),
+                        shape = RoundedCornerShape(12.dp)
                     )
-                },
-                confirmButton = {
-                    Button(
-                        onClick = {
-                            dialogStatus = false
-                            prompt =
-                                "I want to go on a $budget vacation in $month to a $typeSelectedOption destination. " +
-                                        "What is the best US city to visit? Talk about why you chose it for this specific month. " +
-                                        "Limit your answer to 100 words or less. " +
-                                        "Start your response with the city name in between ** and **."
-                            bakingViewModel.sendPrompt(prompt)
-                        }
+                    .padding(8.dp),
+                contentAlignment = Alignment.Center // Center everything vertically and horizontally
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("Select Budget", style = MaterialTheme.typography.bodyLarge)
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    // Center the Text and make it look like a dropdown field
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(0.6f) // Make the dropdown box smaller and centered
+                            .border(
+                                width = 1.dp,
+                                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f),
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .background(MaterialTheme.colorScheme.background)
+                            .clickable { expanded = !expanded }
+                            .padding(8.dp)
                     ) {
-                        Text("I understand")
+                        Text(
+                            text = budget,
+                            modifier = Modifier.align(Alignment.Center),
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
+
+                    // Dropdown menu that matches the width of the dropdown box
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                        modifier = Modifier.width(180.dp) // Make sure the width is similar to the dropdown box
+                    ) {
+                        budgetOptions.forEach { selectionOption ->
+                            DropdownMenuItem(
+                                text = { Text(selectionOption) },
+                                onClick = {
+                                    budget = selectionOption
+                                    expanded = false
+                                }
+                            )
+                        }
                     }
                 }
-            )
-        }
-
-        if (uiState is UiState.Loading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-        } else {
-            var textColor = MaterialTheme.colorScheme.onSurface
-            if (uiState is UiState.Error) {
-                textColor = MaterialTheme.colorScheme.error
-                result = (uiState as UiState.Error).errorMessage
-            } else if (uiState is UiState.Success) {
-                textColor = MaterialTheme.colorScheme.onSurface
-                result = (uiState as UiState.Success).outputText
             }
-            val scrollState = rememberScrollState()
 
-            // parse the response to extract the city name
-            // city name is in between ** and **
-            val cityMatch = Regex("\\*\\*(.*?)\\*\\*").find(result)
-            val cityName = cityMatch?.groupValues?.get(1) ?: ""
-            // remove ** and ** from the result
-            result = result.replace("**$cityName**", cityName)
-            Text(
-                text = cityName,
-                textAlign = TextAlign.Start,
-                color = textColor,
+
+
+            // Type Options Panel
+            Box(
                 modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-            )
-            Text(
-                text = result,
-                textAlign = TextAlign.Start,
-                color = textColor,
+                    .fillMaxWidth()
+                    .padding(4.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.surface.copy(alpha = 0.8f),
+                        shape = RoundedCornerShape(12.dp)
+                    )
+                    .padding(4.dp)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(), // Ensure the entire column takes the full width
+                    horizontalAlignment = Alignment.Start // Align radio buttons to the start
+                ) {
+                    // Center the "Select Type" text
+                    Box(
+                        modifier = Modifier.fillMaxWidth(), // Take full width to allow text to be centered
+                        contentAlignment = Alignment.Center // Center the title text
+                    ) {
+                        Text("Select Type", style = MaterialTheme.typography.bodyLarge)
+                    }
+
+                    // Keep the radio buttons aligned to the start
+                    typeOptions.forEach { text ->
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            RadioButton(
+                                selected = (text == typeSelectedOption),
+                                onClick = { typeSelectedOption = text }
+                            )
+                            Text(text)
+                        }
+                    }
+                }
+
+            }
+
+            // Centered Button Panel
+            Box(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(scrollState)
+                    .fillMaxWidth()
+                    .padding(4.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(
+                    onClick = {
+                        dialogStatus = true
+                    }
+                ) {
+                    Text("Find My Destination")
+                }
+            }
+
+            val monthMap = mapOf(
+                1 to "January",
+                2 to "February",
+                3 to "March",
+                4 to "April",
+                5 to "May",
+                6 to "June",
+                7 to "July",
+                8 to "August",
+                9 to "September",
+                10 to "October",
+                11 to "November",
+                12 to "December"
             )
+
+            // Alert Dialog
+            if (dialogStatus) {
+                AlertDialog(
+                    onDismissRequest = { dialogStatus = true },
+                    title = { Text("AI is not perfect") },
+                    text = { Text("Gemini can make mistakes. Check important info.") },
+                    confirmButton = {
+                        Button(
+                            onClick = {
+                                dialogStatus = false
+
+                                // Get the month name from the map using the month integer
+                                val monthName = monthMap[month] ?: "Unknown month" // Fallback in case month is out of range
+
+                                prompt =
+                                    "I want to go on a $budget vacation in $monthName to a $typeSelectedOption destination. " +
+                                            "What is the best US city to visit? Talk about why you chose it for this specific month. " +
+                                            "Limit your answer to 100 words or less. " +
+                                            "Start your response with the city name in between ** and **."
+                                bakingViewModel.sendPrompt(prompt)
+                            }
+                        ) {
+                            Text("I understand")
+                        }
+                    }
+                )
+            }
+
+            // Result Panel
+            if (uiState is UiState.Loading)
+            {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+            } else {
+                var textColor = MaterialTheme.colorScheme.onSurface
+                if (uiState is UiState.Error) {
+                    textColor = MaterialTheme.colorScheme.error
+                    result = (uiState as UiState.Error).errorMessage
+                } else if (uiState is UiState.Success) {
+                    textColor = MaterialTheme.colorScheme.onSurface
+                    result = (uiState as UiState.Success).outputText
+                }
+                val scrollState = rememberScrollState()
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(4.dp)
+                        .verticalScroll(scrollState)
+                        .background(
+                            color = MaterialTheme.colorScheme.surface.copy(alpha = 0.8f),
+                            shape = RoundedCornerShape(12.dp)
+                        )
+                        .padding(4.dp)
+                ) {
+                    val cityMatch = Regex("\\*\\*(.*?)\\*\\*").find(result)
+                    val cityName = cityMatch?.groupValues?.get(1) ?: ""
+                    result = result.replace("**$cityName**", cityName)
+
+                    Text(
+                        text = cityName,
+                        textAlign = TextAlign.Center,
+                        color = textColor,
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    )
+                    Text(
+                        text = result,
+                        textAlign = TextAlign.Start,
+                        color = textColor,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(top = 8.dp)
+                    )
+                }
+            }
         }
     }
 }
